20a0e75fda344eb897966f7105e23931a5ffe689
Jawad Boulmal <jawadboulmal@gmail.com>
2024-11-04 20:42:31 +0100

    style: refine design element for improved visual consistency

--- assets/js/DeleteDrag.js
+++ assets/js/DeleteDrag.js
@@ -4,22 +4,13 @@ for (const task of tasks) {
     task.addEventListener("dragstart",function(e){    
         console.log(e)
         let idKey = e.target.id;
-        let selectedTask = e.target;
-
-        let taskItems = localStorage.getItem("task_"+idKey)
-        let theObjectTask = JSON.parse(taskItems);
-
 
         TrashButtonUnder.addEventListener("dragover",function(e){
             e.preventDefault();
         })
         TrashButtonUnder.addEventListener("drop",function(e){
-            console.log("deleted")
             ConfirmationPoppup(idKey)
         })
-
-
-
     })
 }
 
